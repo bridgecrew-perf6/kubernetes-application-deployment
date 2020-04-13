fc3e9a7c12d6eb44f23f8b013b29f7c62f177f89
mahmad752 <m.ahmad@cloudplex.io>
2020-04-13 19:00:11 +0500

    fix storageclass deployment issue

--- kubernetes-custom-apis/core/v1/types.go
+++ kubernetes-custom-apis/core/v1/types.go
@@ -1,18 +1,30 @@
 package v1
 
-import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
 
 type RuntimeConfig struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
-	Spec              interface{} `json:"spec"`
-	Data              interface{} `json:"data,omitempty"`
-	Type              interface{} `json:"type,omitempty"`
-	Status            interface{} `json:"status,omitempty"`
-	Rules             interface{} `json:"rules,omitempty"`
-	RoleRef           interface{} `json:"roleRef,omitempty"`
-	Subjects          interface{} `json:"subjects,omitempty"`
-	StringData        interface{} `json:"stringData,omitempty"`
+	Spec              interface{}       `json:"spec"`
+	Data              interface{}       `json:"data,omitempty"`
+	Type              interface{}       `json:"type,omitempty"`
+	Status            interface{}       `json:"status,omitempty"`
+	Rules             interface{}       `json:"rules,omitempty"`
+	RoleRef           interface{}       `json:"roleRef,omitempty"`
+	Subjects          interface{}       `json:"subjects,omitempty"`
+	StringData        interface{}       `json:"stringData,omitempty"`
+	SCprovisioner     string            `json:"provisioner,omitempty"`
+	SCparameters      map[string]string `json:"parameters,omitempty"`
+	SCReclaimPolicy   string            `json:"reclaimPolicy,omitempty" protobuf:"bytes,4,opt,name=reclaimPolicy,casttype=k8s.io/api/core/v1.PersistentVolumeReclaimPolicy"`
+
+	SCMountOptions []string `json:"mountOptions,omitempty" protobuf:"bytes,5,opt,name=mountOptions"`
+
+	SCAllowVolumeExpansion bool `json:"allowVolumeExpansion,omitempty" protobuf:"varint,6,opt,name=allowVolumeExpansion"`
+
+	SCVolumeBindingMode string      `json:"volumeBindingMode,omitempty" protobuf:"bytes,7,opt,name=volumeBindingMode"`
+	SCAllowedTopologies interface{} `json:"allowedTopologies,omitempty" protobuf:"bytes,8,rep,name=allowedTopologies"`
 }
 type RuntimeConfigList struct {
 	metav1.TypeMeta `json:",inline"`
